15bf6445818f304607bd1f90eb0e300b6caf3581
SorenErvik <ervik.soren@gmail.com>
2023-08-15 10:59:20 -0500

    Added solution for 8-15-2023 coding challenge

--- Find_The_Capitals.js
+++ Find_The_Capitals.js
@@ -5,3 +5,17 @@ Write a function that takes a single string (word) as argument. The function mus
 Example (Input --> Output)
 "CodEWaRs" --> [0,3,4,6]
 */
+
+
+var capitals = function (word) {
+	// Write your code here
+  const arr = word.split('');
+  const capitals = [];
+  arr.forEach(function(element) {
+    if (/[A-Z]/.test(element)) {
+      const index = arr.indexOf(element);
+      capitals.push(index);
+    }
+  });
+  return capitals;
+};
