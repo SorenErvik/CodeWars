5e63560e5389ee418510173e4a2b0c938d03900b
SorenErvik <ervik.soren@gmail.com>
2023-08-21 11:38:03 -0500

    Finished my daily CodeWars

---
+++ TrainingOnReverseWords.js
@@ -0,0 +1,15 @@
+/*Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
+
+
+function reverseWords(str) {
+    let newStr = str.split(" ");
+   for (let i = 0; i < newStr.length; i++) {
+    newStr[i] = newStr[i].split("").reverse().join("");
+   }
+   return newStr.join(" ");
+ }
