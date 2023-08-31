0bf61363497503a83cb6d1c08c9b41867cc8d617
SorenErvik <ervik.soren@gmail.com>
2023-08-31 10:20:55 -0500

    Only add every other element to the new array

---
+++ Removing_Elements.js
@@ -0,0 +1,20 @@
+/* 
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+*/
+
+
+function removeEveryOther(arr){
+    //your code here
+    let keep = [];
+    for (let i=0; i<arr.length; i++) {
+      if (i % 2 === 0) {
+        keep.push(arr[i]);
+      }
+    }
+    return keep;
+  }
