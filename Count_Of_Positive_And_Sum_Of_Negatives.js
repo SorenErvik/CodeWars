8efc863d49b6b214e41d1bf8bb3b3d2afbd7b335
SorenErvik <ervik.soren@gmail.com>
2023-10-04 09:11:53 -0500

    Add the number of positives and add the sum of the negatives

---
+++ Count_Of_Positive_And_Sum_Of_Negatives.js
@@ -0,0 +1,32 @@
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+
+function countPositivesSumNegatives(input) {
+    // your code here
+    let results = [];
+    let sumOfPositives = 0;
+    let sumOfNegatives = 0;
+    if (input === null || input.length === 0) {
+      return [];
+    }
+    for (let i=0; i<input.length; i++) {
+      if (input[i] < 0) {
+        sumOfNegatives += input[i];
+      } else if (input[i] > 0){
+        sumOfPositives++;
+      }
+    }
+   
+        results.push(sumOfPositives, sumOfNegatives);
+    return results;
+    
+  }
