a7a61cf489eccf55afdf8683c641a56e4e59033a
SorenErvik <ervik.soren@gmail.com>
2023-11-08 11:47:31 -0600

    Grab the areacode from the string

---
+++ Areacode_Extractor.js
@@ -0,0 +1,20 @@
+/*
+You've got a bunch of textual data with embedded phone numbers. Write a function area_code() that finds and returns just the area code portion of the phone number.
+
+>>> message = "The supplier's phone number is (555) 867-5309"
+>>> area_code(message)
+'555'
+The returned area code should be a string, not a number. Every phone number is formatted like in the example, and the only non-alphanumeric characters in the string are apostrophes ' or the punctuation used in the phone number.
+*/
+
+function areaCode(text) {
+    let index = 0;
+    
+    for (let x of text) {
+      if (x === '(') {
+        // Capture the next three characters
+        return text[index + 1] + text[index + 2] + text[index + 3];
+      }
+      index++;
+    }
+    }
